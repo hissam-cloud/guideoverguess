525d133350d4c839da6dbbfd23ad289d5353f20c
Hissam <hissamemail@gmail.com>
2025-06-06 15:31:53 +0600

    Title fix half of my day gone

--- wp-content/uploads/uag-plugin/assets/2000/uag-js-2094.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
-	UAGBTimelineClasses( {"block_id":"c75cc5b4","timelinAlignment":"center","timelinAlignmentTablet":"left","timelinAlignmentMobile":"left"}, '.uagb-block-c75cc5b4' );
-});
-window.addEventListener("resize", function(){
-	UAGBTimelineClasses( {"block_id":"c75cc5b4","timelinAlignment":"center","timelinAlignmentTablet":"left","timelinAlignmentMobile":"left"}, '.uagb-block-c75cc5b4' );
-});
- });
