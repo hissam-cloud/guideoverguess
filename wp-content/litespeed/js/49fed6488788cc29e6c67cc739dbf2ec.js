86baf563eebeede7a78390cf5dd0ae491cd8554e
Hissam <hissamemail@gmail.com>
2025-06-07 02:01:31 +0600

    new post

---
+++ wp-content/litespeed/js/49fed6488788cc29e6c67cc739dbf2ec.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){window.addEventListener("DOMContentLoaded",function(){UAGBTimelineClasses({"block_id":"c75cc5b4","timelinAlignment":"center","timelinAlignmentTablet":"left","timelinAlignmentMobile":"left"},'.uagb-block-c75cc5b4')});window.addEventListener("resize",function(){UAGBTimelineClasses({"block_id":"c75cc5b4","timelinAlignment":"center","timelinAlignmentTablet":"left","timelinAlignmentMobile":"left"},'.uagb-block-c75cc5b4')})})
+;
